f562ade31db4fe20aef272b49a8f6006ef8555bd
Cyril <cyril@ro.ru>
2014-04-11 00:17:16 +0400

    Moved generator call out of the with block.

--- grigorev/B-2-proper.py
+++ grigorev/B-2-proper.py
@@ -13,12 +13,13 @@ def generator(subsequence, symbols, length):
 with open("vectors.in", "r") as infile:
 	n = int(infile.readline().strip())
 
+sequences = list(generator(
+	subsequence = [],
+	symbols     = ["0", "1"],
+	length      = n
+))
+
 with open("vectors.out", "w") as outfile:
-	sequences = list(generator(
-		subsequence = [],
-		symbols     = ["0", "1"],
-		length      = n
-	))
 	outfile.write(str(len(sequences)) + "\n")
 	for sequence in sequences:
 		outfile.write("".join(sequence) + "\n")
