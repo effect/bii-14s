b78cd748539f23c2da5475d8226cbc2c9e45c209
Cyril <cyril@ro.ru>
2014-04-11 00:28:04 +0400

    B-5 (subsets).

---
+++ grigorev/B-5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from __future__ import division
+
+def generator(subsequence, numbers):
+	yield subsequence
+	for number in numbers:
+		if number > max(subsequence + [-1]):
+			for sequence in generator(subsequence + [number], numbers):
+				yield sequence
+
+with open("subsets.in", "r") as infile:
+	n = int(infile.readline().strip())
+
+subsets = generator(
+	subsequence = [],
+	numbers = list(xrange(1, n+1))
+)
+
+with open("subsets.out", "w") as outfile:
+	for subset in subsets:
+		outfile.write(" ".join([str(x) for x in subset]) + "\n")
