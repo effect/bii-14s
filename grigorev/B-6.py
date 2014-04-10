d7a226c8b06a437cc8b00684debd79549037bbba
Cyril <cyril@ro.ru>
2014-04-11 01:47:37 +0400

    B-6 (nextvector).

---
+++ grigorev/B-6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from __future__ import division
+
+def neighbor(vector, direction):
+	# decide what symbols to consider (P)revious and (N)ext
+	if direction == 1:
+		P, N = ["0", "1"]
+	else:
+		P, N = ["1", "0"]
+	# create a copy of vector
+	result = vector[:]
+	# step-through like a digital clock
+	for pos, val in enumerate(vector):
+		if val == P:
+			result[pos] = N
+			return result
+		else:
+			result[pos] = P
+	# fallback
+	return ["-"]
+
+with open("nextvector.in", "r") as infile:
+	string = infile.readline().strip()
+	vector = list(string)[::-1] # we order positions RTL when interpreting binary
+
+predecessor = neighbor(vector, -1)
+successor   = neighbor(vector, 1)
+
+with open("nextvector.out", "w") as outfile:
+	outfile.write("".join(predecessor[::-1]) + "\n")
+	outfile.write("".join(successor[::-1])   + "\n")
