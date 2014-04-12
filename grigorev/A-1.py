47b5f0160cb84b7b41fd4b05d6da6d2f50cdc3d7
Cyril <cyril@ro.ru>
2014-04-12 11:20:41 +0400

    Refactoring and comments.

--- grigorev/A-1.py
+++ grigorev/A-1.py
@@ -3,23 +3,35 @@ from __future__ import division
 import sys
 from collections import deque
 
-def graphFromData(inputData):
+
+""" Reads input line per line. Expects first line to contain the number of   """
+""" vertices and edges, all following lines are edges in format (u, v).      """
+""" Returns an adjacency list (actually a dictionary).                       """
+def adjacencyListFromData(inputData):
+	# catch-all record
 	graph = lambda: None
-	graph.body = {}
+	# (V)ertices, (E)dges
 	[graph.V, graph.E] = [int(x) for x in inputData[0].split()]
+	# preload adjacency list
+	graph.body = {
+		vertex: []
+		for vertex in xrange(1, graph.V + 1)
+	}
+	# parse input
 	for edgeDescription in inputData[1:]:
+		# undirected graph, draw edges both ways
 		[start, end] = [int(x) for x in edgeDescription.split()]
-		graph.body[start] = graph.body.get(start, []) + [end  ]
-		graph.body[end  ] = graph.body.get(end,   []) + [start]
-	for vertex in range(1, graph.V + 1):
-		if vertex not in graph.body:
-			graph.body[vertex] = []
+		graph.body[start].append(end  )
+		graph.body[end  ].append(start)
 	return graph
 
+
+""" Perform a depth-first search on a graph.                                 """
+""" Return "components" (a dictionary of following structure):               """
+"""        key = vertex                                                      """
+"""        value = number of component to which it belongs                   """
 def depthFirstSearch(graph):
-	# vertex: componentToWhichItBelongs
 	components = {}
-	# assume 0 components initially
 	currentComponent = 0
 	# NB! vertices start from 1 and go up to V inclusive
 	for vertex in range(1, graph.V + 1):
@@ -41,10 +53,12 @@ def depthFirstSearch(graph):
 				queue.popleft()
 	return components
 
+
 def main():
+	# using sys.std* handles for ease of local debugging
 	sys.stdin  = open("components.in",  "r")
 	sys.stdout = open("components.out", "w")
-	graph      = graphFromData(sys.stdin.readlines())
+	graph      = adjacencyListFromData(sys.stdin.readlines())
 	components = depthFirstSearch(graph)
 	# number of components
 	print(max(components.values()))
