6e75d8f069b88f042cd9605e94e85a80b1d13b15
Sofiiako <19rubisco93@gmail.com>
2014-05-02 21:31:13 +0400

    some editing done

--- kolchanova/python3.py
+++ kolchanova/python3.py
@@ -7,10 +7,15 @@ import matplotlib.pyplot as plot
 import random
 %matplotlib inline
 
-x = numpy.linspace(0, 10, 1000)
+x = numpy.linspace(0, 2, 1000)
 y = 2 * x + 1
 noise = scipy.randn(y.size)
 y_noise = y + noise
+
+X_full = numpy.array(x).T
+y_full = numpy.array(y_noise).T
+print (X_full.shape)
+print (y_full.shape)
 
 TRAIN_SIZE = 800
 from sklearn.utils import shuffle
@@ -31,12 +36,15 @@ Test_set = numpy.hstack((X_test,Y_test))
 print (Test_set.shape)
 
 betas = scipy.polyfit(x, y_noise, 1)[::-1]  # 1 is order (max degree of x)
-print (betas)
+print ("betas:", betas)
+print ("original model: y = 2 * x + 1")
+print ("proposed model: y = 1.99725967*x + 1.03406303")
 
-_ = plot.plot(x, y_noise, "r.")
-_ = plot.plot(x, betas[0] + betas[1]*x, color="blue")
+_ = plot.plot(x, y_noise, "r.", label = "full set")
+_ = plot.plot(x, betas[0] + betas[1]*x, color="blue", label = "model")
 plot.xlabel('X')
 plot.ylabel('Y')
+_ = plot.legend(bbox_to_anchor = (1.05, 1), loc = 2, borderaxespad = 0)
 
 from sklearn import linear_model
 regr = linear_model.LinearRegression()
@@ -49,7 +57,8 @@ print (regr.coef_)
 print (regr.intercept_)
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(x,y_noise)
-print ("Full set r-squared (determination coefficient):", r_value**2) 
+print ("Full set r-squared (determination coefficient):", r_value**2)
+print ("standard error = ", std_err)
 
 train_set_error = sqrt(numpy.sum(((slope*x_train+intercept) - y_train)**2)/len(y_train))
 print ('Residual sum of squares, train:', train_set_error)
@@ -64,13 +73,29 @@ pearsonr(x, y_noise) #correlation coefficient x1
 print ("Correlation coefficient array:")
 numpy.corrcoef(x,y_noise) #correlation coefficient x2
 
-_ = plot.plot(x_train, y_train, "r.")
-_ = plot.plot(x_test,y_test, "b.")
-_ = plot.plot(x, betas[0] + betas[1]*x, color="green", linewidth = 3)
+_ = plot.plot(x_train, y_train, "r.", label = "train")
+_ = plot.plot(x_test,y_test, "b.", label  = "test")
+_ = plot.plot(x, betas[0] + betas[1]*x, color="green", linewidth = 3, label = "model")
 plot.xlabel('X')
 plot.ylabel('Y')
+_ = plot.legend(bbox_to_anchor = (1.05, 1), loc = 2, borderaxespad = 0)
 
+model_function = lambda x: betas[0] + betas[1]*x
+ok = []
+too_far = []
+for tx, ty in numpy.vstack((X_full, y_full)).T.tolist():
+    if (model_function(tx) - ty)**2 < std_err:
+        ok.append([tx, ty])
+    else:
+        too_far.append([tx, ty])
 
+ok_points = numpy.array(ok)
+too_far = numpy.array(too_far)
+print (ok_points.shape)
+print (too_far.shape)
 
-
+_ = plot.plot(x, betas[0] + betas[1]*x, color="blue", linewidth = 1, label = "model")
+_ = plot.plot(ok_points[:, 0], ok_points[:, 1], "g.", label = "ok points (dist. < std)")
+_ = plot.plot(too_far[:, 0], too_far[:, 1], "r.", label = "points, that are too far (dist.>std)")
+_ = plot.legend(bbox_to_anchor = (1.05, 1),loc = "best",borderaxespad = 0)
 
