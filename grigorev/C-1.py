40e28b1c5d86e9647ffa6a07a6b5b7694a64d5e8
LankyCyril <cyril@ro.ru>
2014-05-04 02:37:29 +0400

    Ugly, but accepted by PCMS.

--- grigorev/C-1.py
+++ grigorev/C-1.py
@@ -4,21 +4,27 @@ from io import BytesIO
 
 vowels = list("aeiouy")
 
-def anagram_generator(head, letters):
+def anagram_generator(head, letters, letterset, lettercount):
 	if len(head) < len(letters):
-		for letter in sorted(set(letters)): # consider unique letters only
-			if head.count(letter) < letters.count(letter): # not all instances in head
+		for letter in letterset: # consider unique letters only
+			if head.count(letter) < lettercount[letter]: # not all instances in head
 				if len(head) > 0 and head[-1] in vowels and letter in vowels: # consecutive vowels
 					continue
-				for anagram in anagram_generator(head + [letter], letters):
+				for anagram in anagram_generator(head + [letter], letters, letterset, lettercount):
 					yield anagram
 	else:
 		yield head
 
 with open("anagrams.in") as infile:
+#   with BytesIO("catt") as infile:
 	word = infile.readline().strip().lower()
 	letters = sorted(list(word))
 
+lettercount = {
+	letter: letters.count(letter)
+	for letter in set(letters)
+}
+
 with open("anagrams.out", "w") as outfile:
-	for anagram in anagram_generator([], letters):
+	for anagram in anagram_generator([], letters, sorted(set(letters)), lettercount):
 		outfile.write("".join(anagram) + "\n")
