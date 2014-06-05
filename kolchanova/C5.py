5c7433f39cfecd3ff06a30a02e9050d9707a76b1
Julia Repina <juliarepina@Julias-MacBook-Pro.local>
2014-06-06 03:45:57 +0400

    C5

---
+++ kolchanova/C5.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+def piece_sizes(cuts):
+        sorted_cuts = sorted(cuts)
+        return [
+                sorted_cuts[i] - sorted_cuts[i-1]
+                for i in range(1, len(sorted_cuts))
+        ]
+
+with open("maxpiece.in") as infile:
+        n, m, k = [int(x) for x in infile.readline().split()]
+        xcuts = [0, n]
+        ycuts = [0, m]
+        for line in infile:
+                t, v = [int(x) for x in line.split()]
+                if t == 0:
+                        xcuts.append(v)
+                else:
+                        ycuts.append(v)
+
+xcuts.sort()
+xsizes = []
+for i, _ in enumerate(xcuts):
+        if i > 0:
+                xsizes.append(xcuts[i] - xcuts[i-1])
+
+ycuts.sort()
+ysizes = []
+for i, _ in enumerate(ycuts):
+        if i > 0:
+                ysizes.append(ycuts[i] - ycuts[i-1])
+
+biggest_square = min(max(xsizes), max(ysizes))
+
+with open("maxpiece.out", "w") as outfile:
+        outfile.write(str(biggest_square))
